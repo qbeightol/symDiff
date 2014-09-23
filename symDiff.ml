2a79d3ce7dbe76261149953150e5ace0bdc5dbae
Quinn Beightol <qeb2@cornell.edu>
2014-09-23 12:49:41 -0400

    implemented diff

--- symDiff.ml
+++ symDiff.ml
@@ -26,16 +26,26 @@ let simp (e: expr) : expr =
 (*differentiates an expression*)
 let rec diff (e: expr) : expr = 
   match e with
-  | Const  n       -> Const 0.0
-  | Var            -> Const 1.0
-  | Plus  (e1, e2) -> 
-  | Minus (e1, e2) -> 
-  | Mult  (e1, e2) -> 
-  | Div   (e1, e2) -> 
-  | Pow   (e1, e2) -> 
-  | Log    e       -> 
-  | Sin    e       -> 
-  | Cos    e       -> 
+  | Const  n            -> Const 0.0
+  | Var                 -> Const 1.0
+  | Plus  (e1, e2)      -> Plus  (diff e1, diff e2)
+  | Minus (e1, e2)      -> Minus (diff e1, diff e2)
+  | Mult  (e1, e2)      -> Plus  (Mult (diff e1, e2), Mult (e2, diff e1))
+  | Div   (e1, e2)      -> 
+    let numerator   = Minus (Mult (diff e1, e2), Mult (e2, diff e1))
+    and denominator = Pow (e2, Const 2.0)
+    in Div (numerator, denominator) 
+  | Pow   (e1, Const n) -> 
+    Mult  (Const n, Mult (diff e1, Pow (e1, Const (n -. 1.0)))) 
+  | Pow   (e1, e2)      -> 
+    let e1_to_e2        = Pow (e1, e2) 
+    and e2'             = diff e2
+    and log_e1          = Log (e1)
+    and e1'e2_div_by_e1 = Div (Mult (diff e1, e2), e1)
+    in Mult (e1_to_e2, Mult (e2', Mult (log_e1, e1'e2_div_by_e1))) 
+  | Log    e            -> Div   (diff e, e) 
+  | Sin    e            -> Mult  (diff e, Cos e)
+  | Cos    e            -> Mult  (diff e, Mult (Const (-1.0), Sin e))
 
 (*returns a string representing [e]*)
 let to_string (e: expr) : string =
