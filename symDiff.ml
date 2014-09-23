cb620b46b1c5f9d593829b2f166fb279ccbd169f
Quinn Beightol <qeb2@cornell.edu>
2014-09-23 11:57:38 -0400

    added a to_string function

--- symDiff.ml
+++ symDiff.ml
@@ -2,26 +2,64 @@
 (*module for symbolic differentiation*)
 
 type expr = 
-  | Const of float     (*Const  n       represents the coonstant n*)
-  | Var                (*Var            represents the variable x*)
-  | Plus  of exp * exp (*Plus  (e1, e2) represents e1+e2*)
-  | Minus of exp * exp (*Minus (e1, e2) represents e1-e2*)
-  | Mult  of exp * exp (*Mult  (e1, e2) represents e1*e2*)
-  | Div   of exp * exp (*Div   (e1, e2) represents e1/e2*)
-  | Pow   of exp * exp (*Pow   (e1, e2) represents e1^e2*)
-  | Log   of exp       (*Log   e        represents ln(e)*)
-  | Sin   of exp       (*Sin   e        represents sin(e)*)
-  | Cos   of exp       (*Cos   e        represents cos(e)*)
+  | Const of float       (*Const  n       represents the coonstant n*)
+  | Var                  (*Var            represents the variable x*)
+  | Neg   of expr        (*Neg    e       represents the -(e)*)
+  | Plus  of expr * expr (*Plus  (e1, e2) represents e1+e2*)
+  | Minus of expr * expr (*Minus (e1, e2) represents e1-e2*)
+  | Mult  of expr * expr (*Mult  (e1, e2) represents e1*e2*)
+  | Div   of expr * expr (*Div   (e1, e2) represents e1/e2*)
+  | Pow   of expr * expr (*Pow   (e1, e2) represents e1^e2*)
+  | Log   of expr        (*Log   e        represents ln(e)*)
+  | Sin   of expr        (*Sin   e        represents sin(e)*)
+  | Cos   of expr        (*Cos   e        represents cos(e)*)
 
 
 (*produces a symbolic representation of a string*)
-let to_exp (s: string) : expr = 
+let to_expr (s: string) : expr = 
   failwith "not implemented"
+
 
 (*simplifies an expr*)
 let simp (e: expr) : expr = 
   failwith "not implemented"
 
 (*differentiates an expression*)
-let diff (e: expr) : expr = 
+let rec diff (e: expr) : expr = 
+  match e with
+  | Const  n       -> Const 0.0
+  | Var            -> Const 1.0
+  | Neg    e       -> 
+  | Plus  (e1, e2) -> 
+  | Minus (e1, e2) -> 
+  | Mult  (e1, e2) -> 
+  | Div   (e1, e2) -> 
+  | Pow   (e1, e2) -> 
+  | Log    e       -> 
+  | Sin    e       -> 
+  | Cos    e       -> 
+
+(*returns a string representing [e]*)
+let to_string (e: expr) : string =
   failwith "not implemented"
+
+(*also returns a string representing [e], but with parenthesis indicating the 
+  structure of the expression*)
+let rec lispish_to_string (e: expr) : string =
+  match e with
+  | Const  n       -> string_of_float n
+  | Var            -> "x"
+  | Neg    e       -> "-" ^ (lispish_to_string e)
+  | Plus  (e1, e2) -> "(" ^ (lispish_to_string e1) ^ ") + (" 
+                      ^ (lispish_to_string e2) ^ ")" 
+  | Minus (e1, e2) -> "(" ^ (lispish_to_string e1) ^ ") - (" 
+                      ^ (lispish_to_string e2) ^ ")" 
+  | Mult  (e1, e2) -> "(" ^ (lispish_to_string e1) ^ ") * (" 
+                      ^ (lispish_to_string e2) ^ ")" 
+  | Div   (e1, e2) -> "(" ^ (lispish_to_string e1) ^ ") / (" 
+                      ^ (lispish_to_string e2) ^ ")" 
+  | Pow   (e1, e2) -> "(" ^ (lispish_to_string e1) ^ ") ^ (" 
+                      ^ (lispish_to_string e2) ^ ")" 
+  | Log    e       -> "ln("  ^ (lispish_to_string e) ^ ")"
+  | Sin    e       -> "sin(" ^ (lispish_to_string e) ^ ")"
+  | Cos    e       -> "cos(" ^ (lispish_to_string e) ^ ")"
